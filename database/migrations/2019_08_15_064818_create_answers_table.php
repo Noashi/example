d1a3aa5e643cab2d1285bc416ce6c11547d5bdb2
noashi <mrerfrg@gmail.com>
2019-08-15 21:04:51 +0900

    マイグレートファイルを作成

---
+++ database/migrations/2019_08_15_064818_create_answers_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAnswersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('answers', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('fullname', 200);
+            $table->tinyInteger('gender');
+            $table->integer('age_id');
+            $table->string('email', 255);
+            $table->boolean('is_send_email');
+            $table->text('feedback');
+            $table->timestamps();
+            $table->dateTime('deleted_at')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('answers');
+    }
+}
